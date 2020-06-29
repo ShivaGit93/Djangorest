663150c0f26c2298156bb1a4f1d1bbab898604e9
shivam <shivshopextra011@gmai.com>
2020-06-29 15:49:58 +0530

    Added PUT,PATCH,DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     """Test API"""
+
+    serializer_class= serializers.HelloSerializer
     def get(self,request,format=None):
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
@@ -12,3 +18,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self,requst):
+        serializer =self.serializer_class(data=requst.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message= f'Hello {name}'
+            return Response ({'messgae':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
