b2153a55bbb923cfff323dd4b1665c5eacaa6ba4
shivam <shivshopextra011@gmai.com>
2020-07-02 16:08:08 +0530

    AddedPermissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from django.db import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+
 from profiles_api import serializers
+from profiles_api import models
+
 
 
 class HelloApiView(APIView):
@@ -93,6 +99,19 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+
+
+
+
+
 
 
 
