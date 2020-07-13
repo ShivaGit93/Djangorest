f40c8a43a14fb90ba850931d1c01900743864f5d
shivam <shivshopextra011@gmai.com>
2020-07-13 20:28:53 +0530

    AddedProfileApi

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,9 @@ from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 from profiles_api import serializers
 from profiles_api import models
 
@@ -110,6 +113,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     search_fields = ('name','email',)
 
 
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
 
 
 
