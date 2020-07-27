da19e41e53258090b3ef179944051148e4819364
shivam <shivshopextra011@gmai.com>
2020-07-27 16:20:12 +0530

    AddedPermissionas aand Mod Header

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,9 @@ from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
 
+from rest_framework.permissions import IsAuthenticated
+
+
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -121,6 +124,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
