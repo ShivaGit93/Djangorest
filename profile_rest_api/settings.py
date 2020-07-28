d15e94a33cbad67b0afac38824039465ea48fc05
shivam <shivshopextra011@gmai.com>
2020-07-28 17:15:41 +0530

    Whitenoise

--- profile_rest_api/settings.py
+++ profile_rest_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL ='profiles_api.UserProfile'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
